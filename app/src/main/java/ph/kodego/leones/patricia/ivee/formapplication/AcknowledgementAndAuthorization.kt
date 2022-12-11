c4b096f6046efe1c656b7c61c77fe01a20de56c4
PatriciaIvee <patriciaiveeleones@gmail.com>
2022-12-11 21:48:04 +0800

    Initial Commit

---
+++ app/src/main/java/ph/kodego/leones/patricia/ivee/formapplication/AcknowledgementAndAuthorization.kt
@@ -0,0 +1,11 @@
+package ph.kodego.leones.patricia.ivee.formapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AcknowledgementAndAuthorization : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acknowledgement_and_authorization)
+    }
+}
---
+++ app/src/main/java/ph/kodego/leones/patricia/ivee/formapplication/References.kt
@@ -0,0 +1,11 @@
+package ph.kodego.leones.patricia.ivee.formapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class References : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_references)
+    }
+}
